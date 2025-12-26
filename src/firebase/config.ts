ab359a0216fea457a6f08fa4237ec5fde97a6ea0
Flex <flexofficialmail@gmail.com>
2025-12-26 08:10:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5633543872-198dd",
+  "appId": "1:1012859367314:web:f666b302473e7faf345b4b",
+  "apiKey": "AIzaSyBXYaHiP4PRfehngzapB_Zwyge0ivEBi3A",
+  "authDomain": "studio-5633543872-198dd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1012859367314"
+};
