5338bc4c94330348a361c6e7bf8f399bfb192644
Flex <flexofficialmail@gmail.com>
2025-12-26 10:35:36 +0000

    After uploading the invoice, I've gotten the following error; No default

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-5633543872-198dd",
   "appId": "1:1012859367314:web:f666b302473e7faf345b4b",
+  "storageBucket": "studio-5633543872-198dd.appspot.com",
   "apiKey": "AIzaSyBXYaHiP4PRfehngzapB_Zwyge0ivEBi3A",
   "authDomain": "studio-5633543872-198dd.firebaseapp.com",
   "measurementId": "",
